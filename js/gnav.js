224ff5183a5e27fb63fc38f61afebc0ee305599e
Rsurf10p <ryo220k1211@gmail.com>
2021-09-17 00:07:57 +0900

    menubaraaaaaa

--- js/gnav.js
+++ js/gnav.js
@@ -1,6 +1,6 @@
-$(function() {
-	$('.btn_gNav').on("click", function(){
-		$(this).toggleClass('open');
-		$('#gNav').toggleClass('open');
-	});
-});
+function allcheck( tf ) {
+   var ElementsCount = document.sampleform.elements.length; // チェックボックスの数
+   for( i=0 ; i<ElementsCount ; i++ ) {
+      document.sampleform.elements[i].checked = tf; // ON・OFFを切り替え
+   }
+}
