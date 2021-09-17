1f0f1b19e349e8f147896448263a94f8768a5f1e
Rsurf10p <ryo220k1211@gmail.com>
2021-09-18 00:23:18 +0900

    image slider

--- js/gnav.js
+++
@@ -1,6 +0,0 @@
-function allcheck( tf ) {
-   var ElementsCount = document.sampleform.elements.length; // チェックボックスの数
-   for( i=0 ; i<ElementsCount ; i++ ) {
-      document.sampleform.elements[i].checked = tf; // ON・OFFを切り替え
-   }
-}
---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(window).on('load', function () {
+  $('.slider').bxSlider({
+    mode: 'horizontal',
+    useCSS: false,
+    preloadImages:"all",
+    controls: false,
+    auto:true,
+
+  });
+});
