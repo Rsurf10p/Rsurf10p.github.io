25dea94073421341f4b2f3725ba84ea2e3a90730
Rsurf10p <ryo220k1211@gmail.com>
2021-09-27 17:57:26 +0900

    add map切り替え

---
+++ js/maphover.js
@@ -0,0 +1,57 @@
+$(function(){
+  $("#map_komatu").hover(
+      function() {
+        $(".areamap_image").attr("src", "image/map_komatu.png");
+        // $(this).addClass("is-hover");
+      },
+      function() {
+        $(".areamap_image").attr("src", "image/map.png");
+        // $(this).removeClass("is-hover");
+      }
+    );
+
+  $("#map_tatumi").hover(
+      function() {
+        $(".areamap_image").attr("src", "image/map_tatumi.png");
+        // $(this).addClass("is-hover");
+      },
+      function() {
+        $(".areamap_image").attr("src", "image/map.png");
+        // $(this).removeClass("is-hover");
+      }
+    );
+
+  $("#map_utizuma").hover(
+      function() {
+        $(".areamap_image").attr("src", "image/map_utizuma.png");
+        // $(this).addClass("is-hover");
+      },
+      function() {
+        $(".areamap_image").attr("src", "image/map.png");
+        // $(this).removeClass("is-hover");
+      }
+    );
+
+  $("#map_shirahama").hover(
+      function() {
+        $(".areamap_image").attr("src", "image/map_shirahama.png");
+        // $(this).addClass("is-hover");
+      },
+      function() {
+        $(".areamap_image").attr("src", "image/map.png");
+        // $(this).removeClass("is-hover");
+      }
+    );
+
+
+  $("#map_ikumi").hover(
+      function() {
+        $(".areamap_image").attr("src", "image/map_ikumi.png");
+        // $(this).addClass("is-hover");
+      },
+      function() {
+        $(".areamap_image").attr("src", "image/map.png");
+        // $(this).removeClass("is-hover");
+      }
+    );
+});
